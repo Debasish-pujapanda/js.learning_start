148ec17944bcc52f287017151cab4260cb6675d5
Debasish-pujapanda <81896070+Debasish-pujapanda@users.noreply.github.com>
2024-09-18 16:38:30 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 135789
+let accountEmail = "debasishpujapanda@gmail.com"
+var accountpassword = "12345"
+accountcity = "puri"
+
+//accountId = 589
+
+/*
+
+prefer not to use var because of issue in block scope and functional scope
+
+*/
+
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"Use strict"; //Treat all code as a newer version 
+//alert (6 +8) // we are using node js not browser 
+
+
+let name = "Dev"
+let age = 34
+let IsLoggedIn = false
+
+// premetive datatypes
+
+// number => 2 to power 53 
+// bigint 
+// string =>> ""
+// boolean => true / false
+// Null = > standlone value 
+// undefined => 
+// symbol => uniques
+
+
+//objects 
+
+console.log(typeof "Dev"); // To know the type of data types
+console.log(typeof null);// output is object
+console.log(typeof undefined);// output undefined
+
+
+
+
+
