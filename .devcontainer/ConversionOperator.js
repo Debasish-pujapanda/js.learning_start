bfb6ab7951599f0d51f5c53f9042b162ff6f5d6c
Debasish-pujapanda <81896070+Debasish-pujapanda@users.noreply.github.com>
2024-09-21 07:39:16 +0000

    ConversionOperator

--- .devcontainer/ConversionOperator.js
+++ .devcontainer/ConversionOperator.js
@@ -1,10 +1,10 @@
-let score = 32 
+// let score = 32 
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+//console.log(typeof(score));
 
-let ValueInNumber = Number(score)
-console.log(typeof ValueInNumber );
+//let ValueInNumber = Number(score)
+//console.log(typeof ValueInNumber );
 
 // "33" => 33
 // "33abc" => NaN
@@ -14,13 +14,28 @@ console.log(typeof ValueInNumber );
 // "hitesh" => true
 
 
-let IsLoggedIn = 1
-let booleanIsLogged = Boolean(IsLoggedIn)
-console.log(booleanIsLogged);
+//let IsLoggedIn = 1
+//let booleanIsLogged = Boolean(IsLoggedIn)
+//console.log(booleanIsLogged);
 
 
-let someNumber = 33
-let stringNumber = String(someNumber);
+//let someNumber = 33
+//let stringNumber = String(someNumber);
+
+// ******************* operations****************
+
+//let value = 4 
+//let negvalue = -value
+//console.log(negvalue)
+
+let str1 = "hello"
+let str2 = " Dev"
+let str3 = str1+str2
+console.log(str3);
+console.log(2+3)
+console.log("3"+2)
+console.log(1+"2"+"2")
+console.log("3"+1+"4")
 
 
 
