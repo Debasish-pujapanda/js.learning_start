b4fe5f23870ff71c29afdecd0230d0b98740a6ef
Debasish-pujapanda <81896070+Debasish-pujapanda@users.noreply.github.com>
2024-09-21 06:19:04 +0000

    ConversionOperator

---
+++ .devcontainer/ConversionOperator.js
@@ -0,0 +1,106 @@
+let score = 32 
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let ValueInNumber = Number(score)
+console.log(typeof ValueInNumber );
+
+// "33" => 33
+// "33abc" => NaN
+//  "true" => 1
+// "false" => 0
+// "" = false
+// "hitesh" => true
+
+
+let IsLoggedIn = 1
+let booleanIsLogged = Boolean(IsLoggedIn)
+console.log(booleanIsLogged);
+
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
