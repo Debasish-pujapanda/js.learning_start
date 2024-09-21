dd323bfce251fe53676b988dbe4bac59d0c838a7
Debasish-pujapanda <81896070+Debasish-pujapanda@users.noreply.github.com>
2024-09-21 16:36:05 +0000

    datatypes_summary

---
+++ .devcontainer/datatypes_summary.js
@@ -0,0 +1,26 @@
+// primitive data types are always call by values 
+
+// 7 - types : string , number , boolean , null , undefined , symbol , BigInt , 
+
+const score = 100;
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let usermail;
+
+const id = Symbol('123')
+const anotherid =Symbol('123')
+
+console.log(id == anotherid); // ouput will be false
+
+// Reference type(Non_primitive): 
+
+// Array , objects , functions 
+
+let myObj {
+    name:"Dev",
+    age: 22,
+}
+
+
