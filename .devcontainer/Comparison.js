06de5392f8c996d7df5d5ca95e36225054e48313
Debasish-pujapanda <81896070+Debasish-pujapanda@users.noreply.github.com>
2024-09-21 08:01:12 +0000

    comparison.js

---
+++ .devcontainer/Comparison.js
@@ -0,0 +1,11 @@
+console.log("2">1);
+
+
+
+//console.log(null>0);
+//console.log(null>=0);
+//console.log(null==0);   // try to avoid line no 5,6,7 types of comparison
+
+// === it checks the data types also 
+
+console.log("4"===3);
