a9a9e6d8cbb163a3f9877f9c347cd5d4b7f3bc84
Debasish-pujapanda <81896070+Debasish-pujapanda@users.noreply.github.com>
2024-09-24 20:08:40 +0000

    number

---
+++ .devcontainer/num_and_math.js
@@ -0,0 +1,19 @@
+//const score = 500 
+
+const balance = new Number(100)
+//console.log(balance);
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(8))
+
+const othernum = 24673.94856
+//console.log(othernum.toPrecision(3))
+
+const numbers = 289000000000000000
+//console.log(numbers.toLocaleString('en-IN'))
+
+//++++++++++++++++++++++++++Maths+++++++++++++++++++++++++++++
+console.log(Math)
+console.log(Math.abs(-89))
+console.log(Math.round(-89879.9876))
+console.log(Math.ceil(-89.98))
+console.log(Math.floor(9.8))
